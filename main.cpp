e64d6e95c1d49007871e44e9f2f378ad3b92016f
Heider Sanchez <heider.sanchez.pe@gmail.com>
2020-11-17 14:39:47 -0500

    init

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "avl.h"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    AVLTree<int> *bstree = new AVLTree<int>();
+    int n, val;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>val;
+        bstree->insert(val);
+    }
+    
+    char option = argv[1][0];
+    switch (option)
+    {    
+    case '1':
+        bstree->displayPreOrder();
+        break;    
+    case '2':
+        cout << bstree->height();
+        break;  
+    case '3':
+        cout << bstree->isBalanced();
+        break;  
+    case '4':
+        bstree->remove(bstree->minValue());
+        cout << bstree->size();
+        break;
+    case '5':
+        bstree->remove(bstree->maxValue());
+        cout << bstree->height();
+        break;    
+    default:
+        cout << "option not found";
+        break;
+    }
+    cout<<endl;
+}
